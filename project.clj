7c2780146cb5b849482653c54d7d350293e1dd5c
Matt Halverson <mhalverson@onekingslane.com>
2014-08-18 11:13:20 -0700

    Fix project groupname

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject danger-date-expr "0.1.0"
+(defproject com.onekingslane.danger/danger-date-expr "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
