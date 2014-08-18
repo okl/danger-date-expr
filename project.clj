ec22b14fb23a3d2838a5d76a2ebf08e3ebe46050
Matt Halverson <mhalverson@onekingslane.com>
2014-08-18 11:18:38 -0700

    Add url and desc to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.onekingslane.danger/danger-date-expr "0.1.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure library for working with date-expressions."
+  :url "https://github.com/okl/danger-date-expr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
